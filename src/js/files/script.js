9cdaccd035d66999f95344c30846c85a595fe466
Данил Хальпуков <danil@MacBook-Air-Danil.local>
2023-03-13 16:27:27 +0300

    In progress

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,23 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+//========================================================================================================================================================
+
+// Menu items hover
+
+/* const menuItemsWithSubMenu = document.querySelectorAll('.menu__item.menu-item-has-children');
+
+if (menuItemsWithSubMenu.length) {
+    menuItemsWithSubMenu.forEach(item => {
+        item.addEventListener('click', (e) => {
+            if (item.classList.contains('_hover')) {
+                item.classList.remove('_hover')
+            } else {
+                item.classList.add('_hover')
+            }
+            console.log('e: ', e.target);
+            e.preventDefault();
+            item.classList.toggle('_hover');
+        });
+    })
+} */
